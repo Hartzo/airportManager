8d6fee645dcc1dc10bc44f800b26063a00a73c7c
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-09-02 05:09:20 +0200

    Update Airport.java

--- src/progetto/model/Airport.java
+++ src/progetto/model/Airport.java
@@ -1,26 +1,62 @@
 package src.progetto.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+//Creating an Airport Object with the informations provided within the exam files("Traccia A", "Esempi_A")
 public class Airport {
+	//Declaring private variables is better than declaring them public, in order to get them visible we need getter/setter methods
+		private String airport_code;
+		private String country;
+		private int maxAircrafts;
+		 
+		 
+		 public Airport(String code, String country, int max){
+			 this.setAirport_code(code);
+			 this.setCountry(country);
+			 this.setMaxAircrafts(max);
+		 }
 
-    private String code;
-    private String country;
-    private int maxAircrafts;
 
-    public Airport(String code, String country, int max){
-        this.code = code;
-        this.country = country;
-        this.maxAircrafts = max;
-    }
+		public String getAirport_code() {
+			return airport_code;
+		}
 
-    public Airport(String[] airportData) {
-        this.code = airportData[0];
-        this.country = airportData[1];
-        this.maxAircrafts = Integer.parseInt(airportData[2]);
-    }
+
+		public void setAirport_code(String airport_code) {
+			this.airport_code = airport_code;
+		}
+
+
+		public String getCountry() {
+			return country;
+		}
+
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
+
+
+		public int getMaxAircrafts() {
+			return maxAircrafts;
+		}
+
+
+		public void setMaxAircrafts(int maxAircrafts) {
+			this.maxAircrafts = maxAircrafts;
+		}
+		
+		//Using a method to read data from lines of input and at the same time creating an Object that will stored in an arraylist
+		public static Airport ReadingData(String data) {
+			
+        	String[] rowParts = data.split(",");
+        	String code = rowParts[0];
+            String country= rowParts[1];
+            int max = Integer.parseInt(rowParts[2]);
+  
+			return new Airport(code, country, max);
+		}
 
 }
+
+
+//due aeroporti si dicono collegati se appaiono in almeno un viaggio ad esempio:" ci sono 3 aereoporti LAX, SUF e BER " se due di questi 
+//sono andata e ritorno, o viceversa, in almeno un viaggio si dicono collegati quindi +1 al contatore , il task 1 richiede quelli senza collegamento
