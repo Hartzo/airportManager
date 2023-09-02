dda3530d30448d209f53b7c0532675fd8103aeac
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-09-02 05:13:54 +0200

    Update Progetto.java

--- src/progetto/Progetto.java
+++ src/progetto/Progetto.java
@@ -1,63 +1,88 @@
 package src.progetto;
+import java.util.*;
 
-import java.util.Scanner;
-import src.progetto.model.Aircraft;
+import src.progetto.model.*;
+import src.progetto.task.*;
 
-public class Progetto {
+public class progetto {
 
-    public static void main(String[] args) {
-        StringBuilder stringlines = new StringBuilder();
-        int numAircrafts;
-        int numAirports;
-        int numFlights;
-        String code;
-        int passeggeri;
-        int serbatoio;
-        int voli;
+	public static void main(String[] args) {
+	
+		ArrayList<Aircraft> aircrafts_List = new ArrayList<Aircraft>();
+		ArrayList<Airport> airports_List = new ArrayList<Airport>();
+		ArrayList<Flight> flights_List = new ArrayList<Flight>();
+		
+		//Opening a Scanner with a try command is more professional , remember to close the scanner is a good practice
+		 try (Scanner input = new Scanner(System.in)) {
+	            
+	            do {
+		            	String line = input.nextLine();
+		                
+		            	//Naming the array of strings "rows" to remember how many lines of input are going to be afterwards 
+		                String[] rows = line.split(" ");
+		                int numAircrafts = Integer.parseInt(rows[0]);
+		                int numAirports = Integer.parseInt(rows[1]);
+		                int numFlights = Integer.parseInt(rows[2]);
+		               // System.out.println("\n");
+		                //System.out.println(numAircrafts + " " + numAirports + " " + numFlights);
+		                
+		                //Storing Aircrafts Data reading n lines as many as numAircrafts
+		                for(int i = 0; i < numAircrafts; i++) {
+		                	
+		                	line = input.nextLine();
+		                	aircrafts_List.add(Aircraft.ReadingData(line));
+		                	/*for(Aircraft a : aircrafts_List) {
+		                		System.out.println(a.getAircraft_code() + " " + a.getMaxPassengers() + 
+				                		" " + a.getTankCapacity() + " " + a.getTripsLastYear());
+		                	}*/
+		                	
+		                }
+		                //Storing Airports Data reading n lines as many as numAirports
+		                for(int i = 0; i < numAirports; i++) {
+		                	
+		                	line = input.nextLine();
+		                	airports_List.add(Airport.ReadingData(line));
+		                	/*for(Airport a : airports_List) {
+		                		System.out.println(a.getAirport_code() + " " + a.getCountry() + 
+				                		" " + a.getMaxAircrafts());
+		                	}*/
+		                	
+		                }
+		                //Storing Flight data reading n lines as many numFlights
+		                for(int i = 0; i < numFlights; i++) {
+		                	
+		                	line = input.nextLine();		                	
+		                	flights_List.add(Flight.ReadingData(aircrafts_List, line));
+		                	/*for(Flight a : flights_List) {
+		                		System.out.println(a.getAircraft() + " " + a.getPassengers() + " " + a.getDepartureAirport()
+				                		+ " " + a.getArrivalAirport() + " " + a.getDay());
+		                	}*/
+		                }
+		                
+		                //Reading last line to identify the which task the program has to run 
+		                String[] tasks = input.nextLine().split(" ");
+		              //Calling each method of the tasks , task 2 and 3 have further data to collect
+		                if (tasks[0].equalsIgnoreCase("TASK1")) 
+		                   Task1.task(aircrafts_List, airports_List, flights_List);  
+		                
+		                else if (tasks[0].equalsIgnoreCase("TASK2")) {
+		                	int p = Integer.parseInt(tasks[1]);
+		                	int q = Integer.parseInt(tasks[2]);
+		                	Task2.task(aircrafts_List, airports_List, flights_List, p , q);
+		                }
+		                
+		                else if (tasks[0].equalsIgnoreCase("TASK3")) {
+		                    int new_Flights = Integer.parseInt(tasks[1]);
+		                    Task3.task(aircrafts_List, airports_List, flights_List, new_Flights);
+		                }
+	                
+	            } while (input.hasNextLine());
+	            
+	            
+	      } catch (Exception e) {
+	            System.err.println("Error!" + e.getMessage());
+	        }
+		 
+	}
 
-        try (Scanner input = new Scanner(System.in)) {
-            String line;
-            do {
-                line = input.nextLine();
-                if (line.equalsIgnoreCase("TASK1")) return;
-
-
-                String[] righe = line.split(" ");
-                numAircrafts = Integer.parseInt(righe[0]);
-                numAirports = Integer.parseInt(righe[1]);
-                numFlights = Integer.parseInt(righe[2]);
-                System.out.println("\n");
-                // System.out.println(numAircrafts + " " + numAirports + " " + numFlights);
-
-                for (int i = 0; i < numAircrafts; i++) {
-                    line = input.nextLine();
-                    String[] rigaParts = line.split(" ");
-                    code = rigaParts[0];
-                    passeggeri = Integer.parseInt(rigaParts[1]);
-                    serbatoio = Integer.parseInt(rigaParts[2]);
-                    voli = Integer.parseInt(rigaParts[3]);
-
-                    Aircraft aircraft1 = new Aircraft(code, passeggeri, serbatoio, voli);
-                    System.out.printf("%s %d %d %d",
-                            aircraft1.getCode(),
-                            aircraft1.getMaxPassengers(),
-                            aircraft1.getTankCapacity(),
-                            aircraft1.getTripsLastYear()
-                    );
-//                    System.out.println(aircraft1.aircraft_code + " " + aircraft1.maxPassengers + " " + aircraft1.tankCapacity + " " + aircraft1.tripsLastYear);
-                }
-
-                for (int i = 0; i < numAirports; i++) {
-
-                }
-
-                for (int i = 0; i < numFlights; i++) {
-
-                }
-
-            } while (input.hasNextLine());
-
-        }
-
-    }
 }
