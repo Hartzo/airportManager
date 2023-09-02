5594d8b2494f5cdcf4ce98a4b9ebb3e6a0a94cae
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-09-02 05:10:15 +0200

    Update Flight.java

--- src/progetto/model/Flight.java
+++ src/progetto/model/Flight.java
@@ -1,56 +1,104 @@
 package src.progetto.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-//Creating a Flight Object with the information provided within the exam files("Traccia A", "Esempi_A")
+import java.util.ArrayList;
+
+//Creating a Flight Object with the informations provided within the exam files("Traccia A", "Esempi_A")
 public class Flight {
+	//Declaring private variables is better than declaring them public, in order to get them visible we need getter/setter methods
+		private Aircraft aircraft;
+		//private String aircraft;
+		private int passengers;
+		private String departureAirport; //Airport departureAirport;
+		private String arrivalAirport;  //Airport arrivalAirport;
+		private int day;
+	    
+		public Flight(Aircraft aircraft, int numPassengers, String departure, String arrival, int day){
+	    	this.setAircraft(aircraft);
+	    	this.passengers = numPassengers;
+	    	this.setDepartureAirport(departure);
+	    	this.setArrivalAirport(arrival);
+	    	this.setDay(day);
+	    }
 
-    //Aircraft aircraft;
-    private String aircraft;
-    private int passengers;
-    private String departureAirport; //Airport departureAirport;
-    private String arrivalAirport;  //Airport arrivalAirport;
-    private int day;
+	    //Creating a method to check if a Flight is overboard
+	    public boolean isOverboard(int numPassengers, int MaxPassengers) {
+	    	
+	    	//A simple casting for the percentage
+	    	int capacity = (int)(MaxPassengers*(90.0f/100.0f));
+	    	
+	    	if (numPassengers > capacity)
+			return true;
+	    	else return false;
+	    	
+	    }  
+	    
+	  //Creating a method to check if a Flight is underboard
+	    public boolean isUnderboard(int numPassengers, int MaxPassengers) {
+	    	
+	    	//A simple casting for the percentage
+	    	int capacity = (int)(MaxPassengers*(10.0f/100.0f));
+	    	
+	    	if (numPassengers < capacity)
+			return true;
+	    	else return false;
+	    	
+	    }
 
-    public Flight(String aircraft, int numPassengers, String departure, String arrival, int day) {
-        this.aircraft = aircraft;
-        this.passengers = numPassengers;
-        this.departureAirport = departure;
-        this.arrivalAirport = arrival;
-        this.day = day;
-    }
+		public Aircraft getAircraft() {
+			return aircraft;
+		}
 
-    public Flight(String[] flightData) {
-        this.aircraft = flightData[0];
-        this.passengers = Integer.parseInt(flightData[1]);
-        this.departureAirport = flightData[2];
-        this.arrivalAirport = flightData[3];
-        this.day = Integer.parseInt(flightData[4]);
-    }
+		public void setAircraft(Aircraft aircraft) {
+			this.aircraft = aircraft;
+		}
+		
+		public int getPassengers() {
+			return passengers;
+		}
+		
+		public void setPassengers(int passengers) {
+			this.passengers = passengers;
+		}
 
-    //Creating a method to check if a Flight is overboard
-    public boolean isOverboard(int numPassengers, int MaxPassengers) {
+		public String getDepartureAirport() {
+			return departureAirport;
+		}
 
-        //A simple casting for the percentage
-        int capacity = (int) (MaxPassengers * (90.0f / 100.0f));
+		public void setDepartureAirport(String departureAirport) {
+			this.departureAirport = departureAirport;
+		}
 
-        if (numPassengers > capacity)
-            return true;
-        else return false;
+		public String getArrivalAirport() {
+			return arrivalAirport;
+		}
 
-    }
+		public void setArrivalAirport(String arrivalAirport) {
+			this.arrivalAirport = arrivalAirport;
+		}
 
-    //Creating a method to check if a Flight is underboard
-    public boolean isUnderboard(int numPassengers, int MaxPassengers) {
+		public int getDay() {
+			return day;
+		}
 
-        //A simple casting for the percentage
-        int capacity = (int) (MaxPassengers * (10.0f / 100.0f));
+		public void setDay(int day) {
+			this.day = day;
+		}
 
-        if (numPassengers < capacity)
-            return true;
-        else return false;
-    }
+		//Using a method to read data from lines of input and at the same time creating an Object that will stored in an arraylist
+		public static Flight ReadingData(ArrayList<Aircraft> aircrafts_List, String data) {
+			
+			String[] rowParts = data.split(" ");
+			String aircraftCode= rowParts[0];
+            int numPassengers = Integer.parseInt(rowParts[1]);
+            String departure= rowParts[2];
+            String arrival= rowParts[3];
+            int numDay = Integer.parseInt(rowParts[4]);
+            
+            Aircraft aircraft = Aircraft.getAircraftByCode(aircrafts_List, aircraftCode);
+
+            return new Flight(aircraft, numPassengers, departure, arrival, numDay);
+			
+		}
+	    
 }
